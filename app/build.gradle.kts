60bcacf32341636fa28d5b9d8a5a0af0aa684b4e
lmarthinez <lmartinez@wi-mobile.com>
2024-07-30 17:35:24 -0500

    Configuración inicial de dependencias (Hilt & Material)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.daggerHilt)
 }
 
 android {
@@ -66,4 +68,13 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Extended Icons
+    implementation(libs.androidx.material.icons.extended)
+
+    // Dagger - Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+    ksp(libs.androidx.hilt.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
 }
---
+++ app/src/main/java/com/example/draganddropwithswipe/DragAndDropWithSwipe.kt
@@ -0,0 +1,7 @@
+package com.example.draganddropwithswipe
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DragAndDropWithSwipe : Application()
